14aa212d7e18d179c545561401783b815feedc41
ComputerJunction <ivanharris@Ivans-MacBook-Pro.local>
2015-10-02 13:37:57 -0400

    CHIPS AND CHIPS
    -Toggle classes on denominations
    -Chips will on display if positive
    -Bets zero out if denomination is higher than the bet
    -What else what else hmmm
    -Small detail:Object is defined generally and array forEach is used
    -Big Deal: total and bank observables were moved into chip

--- scripts/BoardJS/BoardJSredux.js
+++ scripts/BoardJS/BoardJSredux.js
@@ -27,91 +27,127 @@ var viewModel = function() {
 		
 		self.findid = function(data, event){
 			var target = event.target || event.scrElement;
-			var id = target.getAttribute('id');
+			var id = target.getAttribute('id'),
+				denom = {'five':5 , 'twentyfive':25, 'onehundred':100};
 			
-			if(['five', 'twentyfive', 'onehundred'].indexOf(id) != -1) {
-				var den = {'five':5 , 'twentyfive':25, 'onehundred':100};
-				self.denomination(den[id]);
-				var circle = d3.select('svg').select("." + id);
-				circle.classed('clicked', !circle.classed('clicked'));
-			
+			if(denom.hasOwnProperty(id)) {
+				
+				self.denomination(denom[id]);
+				
+				self.svgtogclass(id, Object.keys(denom), 'clicked');
+				
 			}else if (id ==='remove'){
+				
 				self.removal(!self.removal());
-				var circle = d3.select('svg').select("." + id);
-				circle.classed('clicked', !circle.classed('clicked'));
+				self.svgtogclass(id, null, 'clicked');
 				
 			}else{
-				var wager = (self.removal()) ?  -self.denomination(): self.denomination();
-				self.addbet(id,target,wager);
-			}
+				var wager = self.denomination();
+				
+				self.addbet(id, target, wager)
+			};
 		};
 		self.addbet = function (id, target, wager){
 			
 			if (jsondata[id] && wager !=0){
 				
-				self.ifarray(jsondata[id]['Loss'], -wager);
-
-				self.ifarray(jsondata[id]['Win'], Math.round(wager * jsondata[id]['Payout']));
+				var wager = wager,
+					winwager = Math.round(wager * jsondata[id]['Payout']);
 				
-				self.chip(id, target, wager);
-			
-				self.total(self.total()+wager)
+				if(self.removal()) {
+					wager = -wager; winwager = -winwager;
+				};
+				
+				var checkwager = self.chip(id, target, wager);
+				
+				if(wager < 0 && Math.abs(wager) > checkwager) {
+					wager = -checkwager;
+					winwager = -Math.round(checkwager * jsondata[id]['Payout']);
+				}
+				
+				if(wager != 0){
+					self.addbettoobj(jsondata[id]['Loss'],wager);
+					self.addbettoobj(jsondata[id]['Win'], winwager);
+					//update total bets and bankroll
+					self.total(self.total()+wager);
+					self.bank(self.bank()-wager);
+					self.bank() < 4 ? alert("Remove some bets to continue") : null;
+				};
+				
+								 
 			}
 			
-		}
+		};
 		
-		self.ifarray = function(array, wager){
-			array.isArray ?
-				
-			array.forEach(function (element, index){
-				self.addbettoobj(element, wager);
-			})
-			:
-				self.addbettoobj(array, wager);
-		}
+		self.addbettoobj = function(arr, wager){
 			
-		self.addbettoobj = function(element, wager){
-			
-			betobj.hasOwnProperty(element) ? 
+			arr.forEach(function (item, index, array){
+					
+				if(!betobj.hasOwnProperty(item)) {
+					Object.defineProperty(betobj, item, {value:null, writable:true})
+					
+				};
 				
-				betobj[element] += wager
-				
-			:
-				Object.defineProperty(betobj, element, {value:wager, writable:true})
-				
-//			betobj[element] < 4 && (betobj[element] = 0); 
+				betobj[item] += wager
+			});
 				
 			console.info(betobj);
 			
 		};
 
-		
 		self.chip = function(id,target,wager){
 			
 			var svg_loc = d3.select('svg'),
-				chip_class = "Chip__" + id;
-			
+				chip_class = "Chip__" + id,
+				checkwager = wager;
+			//Chip is classed for easy removal
 			var chip = svg_loc.select("." + chip_class);
 			
+			//if chip is in the nest array or not
 			if(!chip[0][0]){
-				var x = target.x.baseVal.value + target.width.baseVal.value / 2,
-					y = target.y.baseVal.value + target.height.baseVal.value / 2;
+				//will the wager be negative or positve
+				if(wager > 0){
+					//put the chip on the screen
+					var x = target.x.baseVal.value + target.width.baseVal.value / 2,
+						y = target.y.baseVal.value + target.height.baseVal.value / 2;
+
+					svg_loc.append('use')
+						.datum(wager)
+						.attr({
+							"xlink:href":"#Chip",
+							"x":x, 
+							"y":y})
+						.classed(chip_class, true);
+
+				}else{
+					//make sure nothing is put in the object of bets
+					checkwager = 0;
+				};
 				
-				svg_loc.append('use')
-					.datum(wager)
-					.attr({
-						"xlink:href":"#Chip",
-						"x":x, 
-						"y":y})
-					.classed(chip_class, true);
 			}else{
-				chip.datum(function(d,i){return wager + d;})
-			}
+					//update data for accurate counting of wager on specific bet
+					chip.datum(function(d,i){checkwager = d; console.log(d); return wager + d;})
+					//remove if zero
+					if(chip.datum() <= 0){
+						chip.remove();
+					};
+			};		
+			return checkwager;
 			
-			if(svg_loc.select("." + chip_class).datum()  == 0){
-				chip.remove();
+		};
+		self.svgtogclass = function(id, arr, classname){
+			var svg = d3.select('svg');
+			//takes array and removes a class
+			if(Array.isArray(arr)){
+				arr.forEach(function(item, index, array){
+					var needclass = svg.select("." + item);
+					needclass.classed(classname, false);
+				});
 			};
-				
+			//toggles the class on the lone target
+			svg.select("." + id).classed(classname, !svg.select("." + id).classed(classname));
+				   
+				   
 		};
 			
 	}
