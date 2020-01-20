0f650af3a80875ac923b36535a3b9347227da980
Ivan Harris <ComputerJunction@users.noreply.github.com>
2020-01-19 20:22:48 -0500

    Correction Correction Correction cross functionality
    add and remove bets

--- scripts/BoardJS/BoardJSredux.js
+++ scripts/BoardJS/BoardJSredux.js
@@ -573,7 +573,7 @@ window.onload = function () {
 			if(self.crosset()){
 				
 				for (bet of cross){
-					if (bet === "Field) {
+					if (bet === "Field") {
 						self.denomination(10)
 					}else{
 						self.denomination(30)
